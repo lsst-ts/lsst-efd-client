dba5cdd7f40d1bcc38f9a7fce97ac312c211072c
Valerie Becker <29263449+Vebop@users.noreply.github.com>
2025-05-08 21:16:35 -0700

    Change dashes to underscores

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ import setuptools_scm
 
 setuptools.setup(
     version=setuptools_scm.get_version(
-        write_to="src/lsst-efd-client/version.py"
+        write_to="src/lsst_efd_client/version.py"
     )
 )
