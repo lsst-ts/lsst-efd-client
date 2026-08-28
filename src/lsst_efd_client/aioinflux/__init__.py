545b066242c6a5d8d5e6d055aa4fecb5b76091ff
Valerie Becker <29263449+Vebop@users.noreply.github.com>
2026-08-28 11:31:45 -0700

    Add aioinflux note and license to aioinflux/__init__.py

--- src/lsst_efd_client/aioinflux/__init__.py
+++ src/lsst_efd_client/aioinflux/__init__.py
@@ -19,6 +19,32 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <https://www.gnu.org/licenses/>.
 
+# aioinflux directory contains code from the aioinflux project, which is
+# licensed under the MIT license.
+# The original code can be found at https://github.com/gusutabopb/aioinflux/
+
+# MIT License
+
+# Copyright (c) 2017 Gustavo Bezerra
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 # flake8: noqa
 from . import serialization
 from .client import InfluxDBClient, InfluxDBError, InfluxDBWriteError
